5e88431557b0b97f97ef2b973e5b7f140c08d927
Stéven Toupin <steven.toupin@gmail.com>
2017-11-21 18:26:45 +0100

    no path fix

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ int	main(void)
 	int			err;
 	t_lem_in	lem_in;
 	int			len;
+	int			no_path_found;
 
 	init(&lem_in);
 	lem_in.verbose = 1;
@@ -16,18 +17,23 @@ int	main(void)
 	}
 	else
 	{
-		solve_bfs(&lem_in);
+		no_path_found = solve_bfs(&lem_in);
 		print_room_list(&(lem_in.rooms));
 		ft_putstr("Start: ");
 		print_room(lem_in.start);
 		ft_putstr("End: ");
 		print_room(lem_in.end);
 		ft_putstr("\n");
-		ft_putstr("Shortest path:\n");
-		len = print_path_from_end(lem_in.end);
-		ft_putstr("Path length: ");
-		ft_putnbr(len);
-		ft_putstr("\n");
+		if (no_path_found)
+			ft_putstr("No path found!\n");
+		else
+		{
+			ft_putstr("Shortest path:\n");
+			len = print_path_from_end(lem_in.end);
+			ft_putstr("Path length: ");
+			ft_putnbr(len);
+			ft_putstr("\n");
+		}
 		clean(&lem_in);
 	}		
 }
